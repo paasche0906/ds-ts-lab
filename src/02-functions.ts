ffd605f7f128427caeaac48c63610e99aec69fda
unknown <jiahchengpan@gmail.com>
2025-01-23 15:04:21 +0000

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,5 +1,5 @@
 import { friends, colleagues } from './01-basics'
-import { Friend, Colleague } from './myTypes'
+import { Friend, Colleague, EmailContact } from './myTypes'
 
 function older(f: Friend): string {
     f.age += 1
@@ -18,12 +18,13 @@ function allOlder(f: Friend[]): string[] {
 console.log(allOlder(friends))
 
 // Find the colleague with the highest extension number.
-function highestExtension(cs: Colleague[]): Colleague {
+function highestExtension(cs: Colleague[]) { // Inferred retun type
     const result = cs.sort(
         (c1, c2) => c1.contact.extension - c2.contact.extension
     );
     return result[cs.length - 1];
 }
+
 console.log(highestExtension(colleagues.current));
 
 function addColleague(cs: Colleague[], name: string, department: string, email: string): void {
@@ -40,6 +41,27 @@ function addColleague(cs: Colleague[], name: string, department: string, email: 
 
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
+function sortColleagues(
+    colleagues: Colleague[],
+    sorter: (c1: Colleague, c2: Colleague) => number
+): EmailContact[] {
+    const sorted = colleagues.sort(sorter); // Colleague[] inferred
+    const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+    return result
+}
+
+console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+function findFriends(friends: Friend[], predicate: (f: Friend) => boolean): Friend[] {
+    return friends.filter(predicate);
+}
+
+console.log(findFriends(friends, (friend) => friend.name.startsWith('Z')));
+console.log(findFriends(friends, (friend) => friend.age < 35));
+
+
 
 
 
--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -17,3 +17,9 @@ export interface ColleagueHistory {
   current: Colleague[],
   former: Colleague[]
 }
+
+export interface EmailContact {
+    name: string;
+    email: string
+}
+
