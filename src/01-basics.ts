4b863ad4208622d4b32c1f8b7bd146c0e422784a
unknown <jiahchengpan@gmail.com>
2025-01-23 14:09:04 +0000

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -13,52 +13,59 @@ interface Colleague {
     }
 }
 
+interface ColleagueHistory {
+    current: Colleague[],
+    former: Colleague[]
+}
+
 const friend1 = {
     name: "Futong Zhu",
     phone: "087-12345",
     age: 23,
-  };
-  
-  const friend2 = {
+};
+
+const friend2 = {
     name: "Zixin Wang",
     phone: "086--12345",
     age: 23,
-  };
-  
-  const friends = [friend1, friend2];
-  console.log(friends[0]);
-  
-  //   -------------------
-  const colleague1 = {
+};
+
+const friends = [friend1, friend2];
+console.log(friends[0]);
+
+//   -------------------
+const colleague1 = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
-      email: "rgraham@company.com",
-      extension: 121,
+        email: "rgraham@company.com",
+        extension: 121,
     },
-  };
-  
-  const colleague2 = {
+};
+
+const colleague2 = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
-      email: "pburke@company.com",
-      extension: 132,
+        email: "pburke@company.com",
+        extension: 132,
     },
-  };
-  
-  const colleague3 = {
+};
+
+const colleague3 = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
-      email: "dos@company.com",
-      extension: 125,
+        email: "dos@company.com",
+        extension: 125,
     },
-  };
-  const colleagues = {
+};
+export const colleagues: ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
-  };
-  
-  console.log(colleagues.current[0]);
-  
+};
+
+
+console.log(colleagues.current[0]);
+
+
